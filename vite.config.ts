8db83186a0936682a5f6674200538ff5c7007b98
ASRathore0 <asingh68416@gmail.com>
2025-06-02 23:06:30 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Lost-and-Found-Web-App/', // 👈 Add this line with your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
